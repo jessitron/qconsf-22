e25cea4b03d7d2fcff242ab4195431e646d1ec52
Jessica Kerr <jessitron@honeycomb.io>
2022-10-20 17:08:29 -0500

    Add OTel logging to frontend

--- src/frontend/utils/telemetry/Instrumentation.js
+++ src/frontend/utils/telemetry/Instrumentation.js
@@ -3,6 +3,9 @@ const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc')
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { NodeSDK, api } = require('@opentelemetry/sdk-node');
 
+// set log level to DEBUG for a lot of output
+api.diag.setLogger(new api.DiagConsoleLogger(), api.DiagLogLevel.DEBUG);
+
 api.propagation.setGlobalPropagator(
   new CompositePropagator({
     propagators: [new W3CBaggagePropagator(), new W3CTraceContextPropagator()],
