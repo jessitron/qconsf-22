094e460c9af62528dc2a65eeaaa0da778159d4f4
Jessica Kerr <jessitron@gmail.com>
2022-10-26 17:25:18 +0000

    wait this too

--- src/paymentservice/fraud.js
+++ src/paymentservice/fraud.js
@@ -4,23 +4,23 @@ const otel = require('@opentelemetry/api');
 
 const logger = pino();
 
-const NoResult = {};
+const NoResult = {message: "no info"};
 
 module.exports.fraudCheck = async request => {
     console.log("Fraud check time: " + request)
     const span = otel.trace.getActiveSpan();
 
     try {
-       const result = await axios.get("http://try-this-whataservice:8080");
-       if (result.status !== 200 ) {
-        logger.warn("Got status: " + result.status);
-        span?.addAttribute("app.fraudstatus", result.status)
-          return NoResult;
-       }
-       const fraudOpinion = result.data;
-       logger.info("Result of fraud check: " + JSON.stringify(fraudOpinion));
-       span?.addAttribute("app.fraud.result", JSON.stringify(fraudOpinion));
-       return { sus: false };
+        const result = await axios.get("http://try-this-whataservice:8080");
+        if (result.status !== 200 ) {
+            logger.warn("Got status: " + result.status);
+            span?.setAttribute("app.fraudstatus", result.status)
+            return NoResult;
+        }
+        const fraudOpinion = result.data;
+        logger.info("Result of fraud check: " + JSON.stringify(fraudOpinion));
+        span?.setAttribute("app.fraud.result", JSON.stringify(fraudOpinion));
+        return { sus: false };
     } catch (err) {
         logger.error(err);
         logger.warn("Unable to check fraud, dependency unavailable");
