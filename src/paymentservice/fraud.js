5dbda93b9687376d1a5684cf384b34dfa2dce5e9
Jessica Kerr <jessitron@gmail.com>
2022-10-28 00:26:37 +0000

    can we get the active span

--- src/paymentservice/fraud.js
+++ src/paymentservice/fraud.js
@@ -9,26 +9,26 @@ const NoResult = {message: "no info"};
 module.exports.fraudCheck = async request => {
     console.log("what does the request have? " + JSON.stringify(request));
     const span = otel.trace.getActiveSpan();
+    span.setAttribute("app.fraudCheck.start", true);
     logger.info("Did I find a span? " + span?.spanContext().spanId);
 
     const url = process.env.FRAUD_CHECK_URL || "http://qcon-java-team-whataservice:8080/";
     try {
         const result = await axios.post(url, request);
-        span?.setAttribute("app.fraud.httpstatus", result.status)
+        span.setAttribute("app.fraud.httpstatus", result.status)
         if (result.status !== 200 ) {
             logger.warn("Got status: " + result.status);
             return NoResult;
         }
         const fraudOpinion = result.data;
         logger.info("Result of fraud check: " + JSON.stringify(fraudOpinion));
-        span?.setAttribute("app.fraud.result", JSON.stringify(fraudOpinion));
-        return { sus: false };
+        span.setAttribute("app.fraud.result", JSON.stringify(fraudOpinion));
+        return fraudOpinion;
     } catch (err) {
         logger.error(err);
         logger.warn("Unable to check fraud, dependency unavailable");
+        span.setAttribute("app.spanIsRecording", span.isRecording());
         span?.recordException(err);
         return NoResult;
-    }
-
-    
+    }    
 }
